9afdc6921a63bd34fdc792c0134669adb0c14f8a
Cristian <cristiancho2502@gmail.com>
2022-02-14 09:31:11 -0500

    header added

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        clifford: "#da373d",
+        transparent: "transparent",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#00004F",
+        metal: "#565584",
+        lineargradient: " #C4C4C4",
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        bermuda: "#2BE2A0",
+        borderfaq: "#D9DAE6",
+        gradientbanner: "#5B43DF",
+        gradientbannerfinal: "#CF5B92 ",
       },
     },
   },
